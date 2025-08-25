ad4e1ce2480253b1c8ec9055597d01cb83931c82
Joaco.M <joaco.martinez1480@gmail.com>
2025-08-24 22:51:20 -0300

    backend

--- backend/db.js
+++ backend/db.js
@@ -6,6 +6,5 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-
-  ssl: process.env.DATABASE_URL?.includes("localhost") ? false : { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false }
 });
